af1ee3efe256e82b6478388d5e3144717b651407
VebriStudy <vebristudy@gmail.com>
2025-07-01 22:30:17 +0700

    Initial commit for cleaning services

--- clearning_api/settings.py
+++ clearning_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mf-f0fb62d77y%6gdt*$lsa*7-qr5*xt(+@20!-z-dg)-u%583'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'Staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- core/models.py
+++ core/models.py
@@ -5,16 +5,11 @@ class CleaningService(models.Model):
         ('rumah', 'Rumah'),
         ('kantor', 'Kantor'),
     ]
-
     name = models.CharField(max_length=100)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='rumah')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration_minutes = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
 
 class Booking(models.Model):
     STATUS_CHOICES = [
@@ -28,6 +23,3 @@ class Booking(models.Model):
     customer_address = models.TextField()
     booking_date = models.DateTimeField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-
-    def __str__(self):
-        return f"{self.customer_name} - {self.service.name}"
